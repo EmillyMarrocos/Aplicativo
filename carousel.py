2bf758359fcfe14554e069205b6b6ecc99607cc0
EmillyMarrocos <emillymarrocos5@gmail.com>
2024-04-30 10:34:08 -0300

    Quarto commit de Teste_App

--- carousel.py
+++ carousel.py
@@ -1,1 +1,19 @@
+from kivy.app import App
+from kivy.uix.carousel import Carousel
+from kivy.uix.image import AsyncImage
 
+class CarouselApp(App):
+    def build(self):
+        # Criando um Carousel
+        carousel= Carousel(direction= 'right', loop= True)
+
+        # Adicionando imagens ao Carousel
+        imagens = ['OIP.jpg', 'R.jpg', 'R.jpeg']
+        for imagem in imagens:
+            imagem_widget= AsyncImage(source= imagem)
+            carousel.add_widget(imagem_widget)
+
+            return carousel
+        
+if __name__ == "__main__":
+    CarouselApp().run()
